a27f973938312033b720afde75c539c33f1ca0a9
24331a05f8-DSP <24331a05f8@mvgrce.edu.in>
2025-12-15 23:00:21 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+print("pi value :", math.pi)
+val = 36
+print("square root of", val, "is :", math.sqrt(val))
+an = 180
+print("for cos :", an, "is :", math.cos(an))
+print("for sin :", an, "is :", math.sin(an))
+       
